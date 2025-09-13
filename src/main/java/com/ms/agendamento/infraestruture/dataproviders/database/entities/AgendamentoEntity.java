d40bf2cf17ff86d17772e4249d237e9131efe46e
Gustavo <ghustavo516@gmail.com>
2025-09-13 15:42:02 -0300

    feat: Conversão de SQL para NoSQL

--- src/main/java/com/ms/agendamento/infraestruture/dataproviders/database/entities/AgendamentoEntity.java
+++ src/main/java/com/ms/agendamento/infraestruture/dataproviders/database/entities/AgendamentoEntity.java
@@ -2,37 +2,108 @@ package com.ms.agendamento.infraestruture.dataproviders.database.entities;
 
 import com.ms.agendamento.domain.StatusAgendamento;
 import com.ms.agendamento.domain.TipoAtendimento;
-import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 
-@Entity
-@Table(name = "TB_AGENDAMENTO")
+@Document(collection = "agendamentos")
 public class AgendamentoEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
-    private Long pacienteId;
+    private String pacienteId;
 
-    private Long medicoId;
+    private String medicoId;
 
     private LocalDateTime dataAgendamento;
 
-    @Enumerated(EnumType.STRING)
     private TipoAtendimento tipoAtendimento;
 
-    @Enumerated(EnumType.STRING)
     private StatusAgendamento status;
 
     private String observacoes;
 
-    @Column(nullable = false, updatable = false)
+    @CreatedDate
     private Date dataCriacao;
 
-    @Column(nullable = false, updatable = false)
+    @LastModifiedDate
     private Date dataAtualizacao;
 
+    // getters e setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPacienteId() {
+        return pacienteId;
+    }
+
+    public void setPacienteId(String pacienteId) {
+        this.pacienteId = pacienteId;
+    }
+
+    public String getMedicoId() {
+        return medicoId;
+    }
+
+    public void setMedicoId(String medicoId) {
+        this.medicoId = medicoId;
+    }
+
+    public LocalDateTime getDataAgendamento() {
+        return dataAgendamento;
+    }
+
+    public void setDataAgendamento(LocalDateTime dataAgendamento) {
+        this.dataAgendamento = dataAgendamento;
+    }
+
+    public TipoAtendimento getTipoAtendimento() {
+        return tipoAtendimento;
+    }
+
+    public void setTipoAtendimento(TipoAtendimento tipoAtendimento) {
+        this.tipoAtendimento = tipoAtendimento;
+    }
+
+    public StatusAgendamento getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusAgendamento status) {
+        this.status = status;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
 }
